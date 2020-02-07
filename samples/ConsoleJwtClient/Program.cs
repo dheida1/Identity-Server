cc7e2304513752f86147fd051dba9293a4986c4d
P00159894 <dina.heidar@la.gov>
2020-02-07 15:25:34 -0600

    - console poc

--- samples/ConsoleJwtClient/Program.cs
+++ samples/ConsoleJwtClient/Program.cs
@@ -1,12 +1,96 @@
-﻿using System;
+﻿using IdentityModel;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace ConsoleJwtClient
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Console Client Credentials Flow with JWT Assertion";
+
+            var response = await RequestTokenAsync();
+            response.Show();
+
+            Console.ReadLine();
+            await CallServiceAsync(response.AccessToken);
+        }
+
+        static async Task<TokenResponse> RequestTokenAsync()
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:4300");
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            var clientToken = CreateClientToken("client.jwt", disco.TokenEndpoint);
+
+            var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client.jwt",
+                Scope = "api1",
+
+                ClientAssertion =
+                {
+                    Type = OidcConstants.ClientAssertionTypes.JwtBearer,
+                    Value = clientToken
+                }
+            });
+
+            if (response.IsError) throw new Exception(response.Error);
+            return response;
+        }
+
+        static async Task CallServiceAsync(string token)
+        {
+            var baseAddress = "https://localhost:5003";
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = await client.GetStringAsync("identity");
+
+            "\n\nService claims:".ConsoleGreen();
+            Console.WriteLine(JArray.Parse(response));
+        }
+
+        private static string CreateClientToken(string clientId, string audience)
+        {
+            var certificate = new X509Certificate2("client.pfx");
+            var now = DateTime.UtcNow;
+
+            var token = new JwtSecurityToken(
+                    clientId,
+                    audience,
+                    new List<Claim>()
+                    {
+                        new Claim("jti", Guid.NewGuid().ToString()),
+                        new Claim(JwtClaimTypes.Subject, clientId),
+                        new Claim(JwtClaimTypes.IssuedAt, now.ToEpochTime().ToString(), ClaimValueTypes.Integer64)
+                    },
+                    now,
+                    now.AddMinutes(1),
+                    new SigningCredentials(
+                        new X509SecurityKey(certificate),
+                        SecurityAlgorithms.RsaSha256
+                    )
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
         }
     }
 }
